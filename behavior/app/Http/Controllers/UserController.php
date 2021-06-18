0febdfa5c0f4df28fb0e5175b1487b634f6ef477
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-06-18 18:02:26 -0300

    aula 8.6 query builder

--- behavior/app/Http/Controllers/UserController.php
+++ behavior/app/Http/Controllers/UserController.php
@@ -45,12 +45,30 @@ class UserController extends Controller
         //     echo "#{$user->id} Nome: {$user->name} {$user->status_description}<br>";
         // }
 
-        DB::table('users')->where('id','<', '500')->chunkById(100, function($users){
-            foreach($users as $user){
-                echo "#{$user->id} Nome: {$user->name} {$user->status}<br>";
-            }
-            
-            echo "Encerrou um ciclo!<br><br>";
-        });
+        // DB::table('users')->where('id','<', '500')->chunkById(50, function($users){
+        //     foreach($users as $user){
+        //         echo "#{$user->id} Nome: {$user->name} {$user->status}<br>";
+        //     }
+
+        //     echo "Encerrou um ciclo!<br><br>";
+        //     sleep(1);
+        // });
+
+        $users = DB::table('users')
+                    //->whereIn('users.status',[0, 1])
+                    //->whereNotIn('users.status', [0, 1])
+                    //->whereNull('')
+                    ->whereNotNull('users.name')
+                    // ->whereColumn('created_at', '=', 'updated_at')
+                    //->whereDate('updated_at', '>', '2021-06-18')
+                    ->whereDay('updated_at', '=', '01')
+                    ->whereMonth('updated_at', '=', '01')
+                    ->whereYear('updated_at', '=', '2022')
+                    ->whereTime('updated_at', '>', '18:00:00')
+                    ->get();
+
+        foreach($users as $user){
+            echo "#{$user->id} Nome: {$user->name} {$user->status}<br>";
+        }
     }
 }

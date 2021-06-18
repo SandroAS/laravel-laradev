3e80d6dd22486e456b136752ba6280de60e238a8
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-06-18 12:22:27 -0300

    aula 8.3 query builder

--- behavior/app/Http/Controllers/UserController.php
+++ behavior/app/Http/Controllers/UserController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = DB::table('users')
+                    ->select('users.id', 'users.name', 'users.status')
+                    ->where('users.status', '=', '1')
+                    ->orderBy('name','ASC')//DESC e ASC
+                    //->oldest('name')//ASC
+                    //->latest('name')//DESC
+                    //->inRandomOrder()
+                    ->limit(10)
+                    ->offset(10)
+                    ->get();
+
+        foreach($users as $user){
+            echo "#{$user->id} Nome: {$user->name} {$user->status}<br>";
+        }
+    }
 }

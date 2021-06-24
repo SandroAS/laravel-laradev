7905a9313176f8b85dcbb9a1e6c32497f2f7775d
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-06-24 13:22:59 -0300

    aula 12.4 projeto website

--- project_course/app/Http/Controllers/Controller.php
+++ project_course/app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\Seo;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,11 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $seo;
+
+    public function __construct()
+    {
+        $this->seo =new Seo();
+    }
 }
--- project_course/app/Http/Controllers/WebController.php
+++ project_course/app/Http/Controllers/WebController.php
@@ -2,24 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Support\Seo;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function home()
     {
-        return view('front.home');
-
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - UpInside Treinamentos',//Título
+            'Eleita a melhor escola de desenvolvimento, programação e marketing digital em mais de 17 países pela Latin Quality American Institute!',//Descrição
+            url('/'),//Url
+            asset('images/img_bg_1.jpg'),//Image
+        );
+        return view('front.home', [
+            'head' => $head
+        ]);
     }
 
     public function course()
     {
-        return view('front.course');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - Sobre o curso',//Título
+            'Um treinamento completo do zero ao developer para você aprender e se espercializar no Laravel e abrir as portas de um mercado repleto de oportunidades!',//Descrição
+            route('course'),//Url
+            asset('images/img_bg_1.jpg'),//Image
+        );
+        return view('front.course', [
+            'head' => $head
+        ]);
     }
 
     public function blog()
     {
-        return view('front.blog');
+        $head = $this->seo->render(
+            env('APP_NAME') . ' - blog',//Título
+            'Um treinamento completo do zero ao developer para você aprender e se espercializar no Laravel e abrir as portas de um mercado repleto de oportunidades!',//Descrição
+            route('blog'),//Url
+            asset('images/img_bg_1.jpg'),//Image
+        );
+        return view('front.blog', [
+            'head' => $head
+        ]);
     }
 
     public function article()

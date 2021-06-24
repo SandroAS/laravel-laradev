d42cba32c2865bba7a563ba571885f1a325a14a8
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-06-24 17:01:41 -0300

    aula 12.5 projeto website

--- project_course/webpack.mix.js
+++ project_course/webpack.mix.js
@@ -11,5 +11,36 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix
+   .styles([
+      'resources/views/front/assets/css/animate.css',
+      'resources/views/front/assets/css/icomoon.css',
+      'resources/views/front/assets/css/bootstrap.css',
+      'resources/views/front/assets/css/magnific-popup.css',
+      'resources/views/front/assets/css/owl.carousel.min.css',
+      'resources/views/front/assets/css/owl.theme.default.min.css',
+   ], 'public/front/assets/css/vendor.css')
+   
+   .scripts([
+      'resources/views/front/assets/js/modernizr-2.6.2.min.js',
+   ], 'public/front/assets/js/modernizr.js')
+
+   .scripts([
+      'resources/views/front/assets/js/respond.min.js',
+   ], 'public/front/assets/js/respond.min.js')
+
+   .scripts([
+      'resources/views/front/assets/js/jquery.min.js',
+      'resources/views/front/assets/js/jquery.easing.1.3.js',
+      'resources/views/front/assets/js/bootstrap.min.js',
+      'resources/views/front/assets/js/jquery.waypoints.min.js',
+      'resources/views/front/assets/js/jquery.stellar.min.js',
+      'resources/views/front/assets/js/owl.carousel.min.js',
+      'resources/views/front/assets/js/jquery.countTo.js',
+      'resources/views/front/assets/js/jquery.magnific-popup.min.js',
+      'resources/views/front/assets/js/magnific-popup-options.js',
+   ], 'public/front/assets/js/vendor.js')
+   
+   .scripts([
+      'resources/views/front/assets/js/main.js',
+   ], 'public/front/assets/js/main.js');

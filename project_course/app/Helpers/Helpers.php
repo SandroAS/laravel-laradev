b6d579bce01e0ee3a58d01d1b0e22ac4638521c2
Sandro Antônio Souza <sandroantoniosouza98@gmail.com>
2021-06-25 10:55:37 -0300

    aula 12.8 projeto website

---
+++ project_course/app/Helpers/Helpers.php
@@ -0,0 +1,16 @@
+<?php
+if (! function_exists('mix')) {
+    /**
+     * Get the path to a versioned Mix file.
+     *
+     * @param  string  $path
+     * @param  string  $manifestDirectory
+     * @return \Illuminate\Support\HtmlString|string
+     *
+     * @throws \Exception
+     */
+    function mix_e($path)
+    {
+        url(mix($path));
+    }
+}
---
+++ project_course/resources/views/front_old/master/master.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Learn &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="freehtml5.co" />
+
+	{!! $head ?? '' !!}
+
+	{{-- <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800" rel="stylesheet"> --}}
+	<link href="https://fonts.googleapis.com/css2?family=Miriam+Libre:wght@400;700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="{{ mix('front/assets/css/vendor.css') }}">
+	<link rel="stylesheet" href="{{ mix('front/assets/css/style.css') }}">
+
+	<!-- Modernizr JS -->
+	<script src="{{ mix('front/assets/js/modernizr.js') }}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="{{ mix('front/assets/js/respond.js') }}"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	
+    @include('front.includes.header')
+
+    @yield('content')
+
+	@include('front.includes.footer')
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	
+	<script src="{{ mix('front/assets/js/vendor.js') }}"></script>
+	<script src="{{ mix('front/assets/js/main.js') }}"></script>
+
+	</body>
+</html>
+
